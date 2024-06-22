8dbf2bf09310b0c1f8b77a446b85c4624d689b6b
darielgaizta <darielgaz@gmail.com>
2024-06-22 16:07:48 +0700

    add timetable model app

---
+++ src/timetable/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Course(models.Model):
+    """Class that represents course in university."""
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=5, unique=True)
+    credit = models.IntegerField()
+    semester = models.IntegerField()
+    department = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.code
+
+class Location(models.Model):
+    """Class that represents campus location in university."""
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=5, unique=True)
+
+    def __str__(self):
+        return self.code
+
+class Room(models.Model):
+    """Class that represents room in a campus location."""
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=5, unique=True)
+    capacity = models.IntegerField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.code
