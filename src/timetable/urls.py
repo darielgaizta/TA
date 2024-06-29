da043016b419ad912a88b0791c39834622c3fd3e
darielgaizta <darielgaz@gmail.com>
2024-06-29 18:25:40 +0700

    edit build ga view

--- src/timetable/urls.py
+++ src/timetable/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('build/', views.build_all, name='build_all'),
+    path('build/ga', views.build_with_ga, name='build_with_ga'),
 ]
--- src/timetable/views.py
+++ src/timetable/views.py
@@ -5,34 +5,33 @@ from rest_framework.decorators import api_view
 from algorithm.genetic import GeneticTimetable
 from . import models, utils
 
-@api_view(['POST'])
-def build_all(request):
-    if request.method == 'POST':
-        rooms = models.Room.objects.all()
-        courses = models.CourseClass.objects.all()
-        timeslots = models.Timeslot.objects.all()
-        locations = models.Location.objects.prefetch_related(
-            Prefetch('room_set', queryset=models.Room.objects.order_by('id')))
+@api_view(['GET'])
+def build_with_ga(request):
+    rooms = models.Room.objects.all()
+    courses = models.CourseClass.objects.all()
+    timeslots = models.Timeslot.objects.all()
+    locations = models.Location.objects.prefetch_related(
+        Prefetch('room_set', queryset=models.Room.objects.order_by('id')))
 
-        # Fetch Genetic timetable parameters.
-        population_size = request.data.get("population_size")
-        num_generations = request.data.get("num_generations")
+    # Fetch Genetic timetable parameters.
+    population_size = int(request.query_params.get("population_size"))
+    num_generations = int(request.query_params.get("num_generations"))
 
-        # Build Genetic timetable.
-        timetable = GeneticTimetable(
-            rooms=rooms,
-            courses=courses,
-            timeslots=timeslots,
-            population_size=population_size,
-            num_generations=num_generations)
+    # Build Genetic timetable.
+    timetable = GeneticTimetable(
+        rooms=rooms,
+        courses=courses,
+        timeslots=timeslots,
+        population_size=population_size,
+        num_generations=num_generations)
 
-        # Run Genetic algorithm.
-        solution, conflicts = timetable.run()
-        
-        # Write to database Ms. Excel.
-        filename = 'timetable' + str(round(time.time() * 1000))
-        xl = utils.Xl(filename=filename)
-        xl.setup(locations, timeslots)
-        xl.write(solution)
+    # Run Genetic algorithm.
+    solution, conflicts = timetable.run()
+    
+    # Write to database Ms. Excel.
+    filename = 'timetable' + str(round(time.time() * 1000))
+    xl = utils.Xl(filename=filename)
+    xl.setup(locations, timeslots)
+    xl.write(solution)
 
-        return ResponseBuilder.respondWithMessage(status=200, message=f'Success with {conflicts} conflicts.')
+    return ResponseBuilder.respondWithMessage(status=200, message=f'Success with {conflicts} conflicts.')
