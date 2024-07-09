5306e356bdf93e0a9ea0fcb0b7aa13dd2fb33e38
Fatih Darielma Gaitza <darielgaz@gmail.com>
2024-07-09 19:59:11 +0700

    add engine

---
+++ src/engine.py
@@ -0,0 +1,27 @@
+"""
+Author: Dariel Gaizta
+File engine.py is the main part of this application. It contains several functions that take user's input,
+import internal modules, and process them to generate timetable.
+"""
+
+class Engine:
+    def __get_rooms(range: int):
+        pass
+
+    def __get_courses(range: int):
+        pass
+
+    def __get_timeslots(range: int):
+        pass
+    
+    def __get_locations(range: int):
+        pass
+        
+    def __get_lecturers(range: int):
+        pass
+
+    def __get_course_classes(range: int):
+        pass
+    
+    def initialize_timetable():
+        pass
